a2a814df1d87c67fe9bb6845bcc6a3ee7c2d0abd
agui <55447192+GuiWonder@users.noreply.github.com>
2022-06-24 19:17:34 -0500

    Update crfontff.py

--- main/crfontff.py
+++ main/crfontff.py
@@ -1,5 +1,4 @@
 import os, json, fontforge, subprocess, platform
-from collections import defaultdict
 
 pydir = os.path.abspath(os.path.dirname(__file__))
 
@@ -44,7 +43,6 @@ if font.is_cid:
 font.reencode("unicodefull")
 ltb=list()
 for gls in font.glyphs():
-	#if True or gls.unicode<0:
 	if gls.altuni!=None:
 		for alt in gls.altuni:
 			if alt[1]>0:
@@ -76,6 +74,37 @@ for t1 in ltb:
 				l2.append(a2)
 		l2.append((t1[1], 0, 0))
 		font[t1[0]].altuni = tuple(l2)
+print('正在合并多编码汉字...')
+tbmulcod=(('併','倂'), ('醖','醞'), ('輼','轀'), ('嬷','嬤'), ('煴','熅'), ('緼','縕'), ('縆','緪'), ('酝','醞'), ('脱','脫'), ('腽','膃'), ('蒀','蒕'), ('芈','羋'), ('蕰','薀'), ('蜕','蛻'), ('藴','蘊'), ('删','刪'), ('别','別'), ('刹','剎'), ('内','內'), ('册','冊'), ('呐','吶'), ('弑','弒'), ('兑','兌'), ('兖','兗'), ('姗','姍'), ('媪','媼'), ('秃','禿'), ('税','稅'), ('栅','柵'), ('涚','涗'), ('滚','滾'), ('温','溫'), ('捝','挩'), ('悦','悅'), ('榅','榲'), ('愠','慍'), ('敚','敓'), ('氲','氳'), ('揾','搵'), ('棁','梲'), ('俞','兪'), ('值','値'), ('偷','偸'), ('即','卽'), ('告','吿'), ('唧','喞'), ('喻','喩'), ('塈','墍'), ('填','塡'), ('妍','姸'), ('媮','婾'), ('尚','尙'), ('屏','屛'), ('帡','帲'), ('惪','悳'), ('慎','愼'), ('既','旣'), ('暨','曁'), ('概','槪'), ('榆','楡'), ('槙','槇'), ('清','淸'), ('溉','漑'), ('真','眞'), ('研','硏'), ('箳','簈'), ('胼','腁'), ('迸','逬'), ('郎','郞'), ('青','靑'), ('鷏','鷆'), ('俱','倶'), ('查','査'), ('瓶','甁'), ('訮','詽'), ('豜','豣'), ('熙','煕'), ('教','敎'), ('軿','輧')('鄉', '鄕'))
+for chd in tbmulcod:
+	ord(chd[0])
+	if ord(chd[1]) not in font:
+		continue
+	glyt = font[font.findEncodingSlot(ord(chd[1]))]
+	if ord(chd[0]) in font:
+		glyn = font[font.findEncodingSlot(ord(chd[0]))]
+		if glyt.glyphname == glyn.glyphname:
+			continue
+		if glyn.unicode == ord(chd[0]):
+			glyn.unicode = -1
+		elif glyn.altuni != None:
+			l1 = list()
+			for aa in glyn.altuni:
+				if aa[0] != ord(chd[0]):
+					l1.append(aa)
+			if len(l1) > 0:
+				glyn.altuni = tuple(l1)
+			else:
+				glyn.altuni = None
+	print('处理 '+chd[0]+'-'+chd[1])
+	l2=list()
+	if glyt.altuni != None:
+		for a2 in glyt.altuni:
+			l2.append(a2)
+	l2.append((ord(chd[0]), 0, 0))
+	glyt.altuni = tuple(l2)
+
+print('正在设置字体名称...')
 sfntnames=list(font.sfnt_names)
 for jn in sfntnames:
 	if jn[0]=='Japanese':
